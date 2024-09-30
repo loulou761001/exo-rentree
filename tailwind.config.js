e461b1c67a6ca55732f71e07620a3f1f7d89c77a
loulou761001 <louis.cherik@gmail.com>
2024-09-30 13:17:08 +0200

    :sparkles: add greens

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-green': '#c0f497',
+        'darker-green': '#36463D',
+        'dark-green': '#28A263',
+        'medium-green': '#2DDB81'
+      }
+    },
   },
   plugins: [],
 }
